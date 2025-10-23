29eccf7dafd458a395f5e3959955b27814a5282c
skawuma <82976186+skawuma@users.noreply.github.com>
2025-10-23 12:26:33 -0400

    updated environment values in frontend

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
 
-  //dev purporse
- // apiUrl: 'http://Samuels-MacBook-Pro.local:8080/api'
-   // apiUrl: 'http://localhost:8080/api'
-
  apiUrl: 'https://schedule.samuelkawuma.com/api'
 
 };
